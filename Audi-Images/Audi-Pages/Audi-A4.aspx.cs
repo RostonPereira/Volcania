1e17b0b703a4af9c74d26a3f9fa1b1a34988f23b
Roston Pereira <roston.pereira9@gmail.com>
2016-08-12 16:40:35 +0530

    Add files via upload

---
+++ Audi-Images/Audi-Pages/Audi-A4.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Audi_Images_Audi_Pages_Audi_A4 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+
+    }
+    protected void Button6_Click(object sender, EventArgs e)
+    {
+
+        Session["a"] = Label1.Text;
+        Session["a1"] = Label2.Text;
+        Response.Redirect("http://localhost:49347/volcania/Booking.aspx");
+    }
+    protected void Button5_Click(object sender, EventArgs e)
+    {
+
+        Session["a"] = Label1.Text;
+        Session["a1"] = Label2.Text;
+        Response.Redirect("http://localhost:49347/volcania/Customize.aspx");
+    }
+
+
+
+    protected void Button9_Click(object sender, EventArgs e)
+    {
+        string s;
+        s = "<object style='height: 400px; width: 600px' ><param name='movie' value='http://localhost:49347/volcania/360 view/audiA4.swf'/><embed src='http://localhost:49347/volcania/360 view/audiA4.swf' width='600' height='400'></embed></object>";
+        Literal1.Text = s;
+    }
+}
